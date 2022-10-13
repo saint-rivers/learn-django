6aa2eec9d8f85b7fa6a9a7b942b11373e1674775
Dayan Eam <eam.dayan@gmail.com>
2022-10-13 20:18:59 +0700

    fix viewset

--- dailycheck/views.py
+++ dailycheck/views.py
@@ -7,10 +7,19 @@ from .models import Todo
 # Create your views here.
 
 
-class TodoViewSet(viewsets.ViewSet):
+class TodoViewSet(viewsets.ModelViewSet):
     # if request.GET()
     queryset = Todo.objects.all().order_by('date_created')
     serializer_class = TodoSerializer
-    # filter_backends = [filters.SearchFilter]
-    # search_fields = ['task']
-    # get_extra_actions = []
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['task']
+
+# class TodoViewTextSearch(request, viewsets.ReadOnlyModelViewSet):
+#     search_term = request.query_params["user_id"]
+#     queryset = Todo.objects.filter(task__contains=search_term)
+#     serializer_class = TodoSerializer
+
+
+# class ProfileViewSet(viewsets.ModelViewSet):
+#     queryset = Profile.objects.all().order_by('place')
+#     serializer_class = ProfileSerializer
